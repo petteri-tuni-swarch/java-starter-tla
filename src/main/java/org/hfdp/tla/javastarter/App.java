6bb70b2608ed92dc2eb0b6457905480f5277655e
Petteri Jekunen <petteri.jekunen@tuni.fi>
2021-09-01 12:51:57 +0300

    Initial

---
+++ src/main/java/org/hfdp/tla/javastarter/App.java
@@ -0,0 +1,13 @@
+package org.hfdp.tla.javastarter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "=== Welcome to the SW Architecture and Design Course ===" );
+    }
+}
